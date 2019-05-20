62fcfde8117319eab01aec7c6a3a66341fdf7d09
Francisco Hohn <francisco.hohn@gmail.com>
2019-05-20 08:29:35 -0500

    close, but no cigar. can't get anything to happen when i press html button

--- 12-Web-Scraping-and-Document-Databases/app.py
+++ 12-Web-Scraping-and-Document-Databases/app.py
@@ -8,31 +8,23 @@ app = Flask(__name__)
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
+    mars_news = mongo.db.collection.find_one() 
+    return render_template("index.html", mars=mars_news)
 
-    # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", mars=mars_data)
-
-
-# Route that will trigger the scrape function
+# Route to trigger scrape function
 @app.route("/scrape")
 def scrape():
-
+    mars = mongo.db.collection
     # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
-
+    mars_data = scrape_mars.scrape()
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mars.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
